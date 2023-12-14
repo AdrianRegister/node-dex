b316bfe66bb71b092e6fcc48311c19ca41d270d9
Adrian <adrian.register@yahoo.co.uk>
2023-12-14 15:19:38 +0100

    implemented search functionality

--- public/browser-app.js
+++ public/browser-app.js
@@ -4,40 +4,74 @@ const searchButton = document.querySelector('button')
 const backToHomeButton = document.querySelector('.back-to-home')
 const pokemonInfoContainer = document.querySelector('.pokemon-info-container')
 
-getAllPokemon()
+let allPokemonData = []
+initializeData()
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    getOnePokemon()
+    searchResults()
 })
 
 backToHomeButton.addEventListener('click', () => {
+    searchField.value = ''
     backToHomeButton.style.display = 'none'
-    pokemonInfoContainer.innerHTML = ''
-    getAllPokemon()
+    initializeData()
 })
 
-// search functions
-async function getAllPokemon() {
+pokemonInfoContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        const pokemonDiv = e.target.parentNode
+        getOnePokemon(pokemonDiv.dataset.name)
+    }
+})
+
+// make one API call to get all data
+async function initializeData() {
     const {data} = await axios.get('/api/v1/pokemon')
-    for (let i = 0; i < data.length; i++) {
-        const name = capitalizeName(data[i].name)
-        pokemonInfoContainer.innerHTML += `<div class="pokemon-info-div">
-                    <span class="pokemon-number">#${data[i].id} </span><span class="pokemon-name">${name}</span>
-                    <img class="pokemon-image" src="./pokemon-images/${data[i].id}.png">
+    allPokemonData = data
+    getAllPokemon()
+}
+
+// search functions
+function getAllPokemon() {
+    pokemonInfoContainer.innerHTML = ''
+    for (let i = 0; i < allPokemonData.length; i++) {
+        const name = capitalizeName(allPokemonData[i].name)
+        pokemonInfoContainer.innerHTML += `<div class="pokemon-info-div" data-name="${allPokemonData[i].name}">
+                    <span class="pokemon-number">#${allPokemonData[i].id} </span><span class="pokemon-name">${name}</span>
+                    <img class="pokemon-image" src="./pokemon-images/${allPokemonData[i].id}.png">
                     <p class="pokemon-flavor-text"></p>
                 </div>`
     }
 }
 
-async function getOnePokemon() {
-    const {data} = await axios.get(`/api/v1/pokemon/${searchField.value.toLowerCase()}`)
-    searchField.value = ''
+function searchResults() {
+    pokemonInfoContainer.innerHTML = ''
     backToHomeButton.style.display = 'inline'
-    const name = capitalizeName(data.name)
+    let names = allPokemonData.map(pokemon => pokemon.name)
+    const regex = new RegExp(searchField.value, 'i')
+    let matchingNames = names.filter(name => regex.test(name))
+
+    for (i = 0; i < matchingNames.length; i++) {
+        const matchingPokemon = allPokemonData.find(pokemon => pokemon.name === matchingNames[i])
+        const name = capitalizeName(matchingNames[i])
+        pokemonInfoContainer.innerHTML += `<div class="pokemon-info-div" data-name="${matchingPokemon.name}">
+                    <span class="pokemon-number">#${matchingPokemon.id} </span><span class="pokemon-name">${name}</span>
+                    <img class="pokemon-image" src="./pokemon-images/${matchingPokemon.id}.png">
+                    <p class="pokemon-flavor-text"></p>
+                </div>`
+    }
+}
+
+// when pokemon image clicked, this displays additional info
+function getOnePokemon(pokeName) {
+    pokemonInfoContainer.innerHTML = ''
+    backToHomeButton.style.display = 'inline'
+    const singlePokemon = allPokemonData.find(pokemon => pokemon.name === pokeName)
+    const name = capitalizeName(pokeName)
     pokemonInfoContainer.innerHTML = `<div class="single-pokemon-info-div">
-                    <span class="pokemon-number">#${data.id} </span><span class="pokemon-name">${name}</span>
-                    <img class="pokemon-image" src="./pokemon-images/${data.id}.png">
+                    <span class="pokemon-number">#${singlePokemon.id} </span><span class="pokemon-name">${name}</span>
+                    <img class="pokemon-image" src="./pokemon-images/${singlePokemon.id}.png">
                     <p class="pokemon-flavor-text"></p>
                 </div><br>`
 }
